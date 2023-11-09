9116fc03dfc96cbb74c343c7dcb17049a7560915
Nick Brandis <niklbrandis@gmail.com>
2023-11-08 18:24:32 -0800

    merged speech processing and bear audio

--- game.py
+++ game.py
@@ -1,5 +1,14 @@
 import speech_processing as sp
+import text_to_bear_audio as tba
 
-if __name__ == '__main__':
+def main():
     speech = sp.gather_speech_data()
     print("you said: " + speech)
+
+    # TODO: Make API call
+
+    tba.convert_text_to_bear_audio(speech)
+
+
+if __name__ == '__main__':
+    main()
--- text_to_bear_audio.py
+++ text_to_bear_audio.py
@@ -10,8 +10,6 @@
 #
 ###################################################################################################################
 
-input_text = "Hi there! What's your name? My name is foo foo the bear. Would you like to play a choose your own adventure game?"
-
 from gtts import gTTS
 from pydub import AudioSegment
 import numpy as np
@@ -19,35 +17,48 @@ from numpy.random import uniform
 import wave
 import os
 
-int_dir = "intermediate_files"
-out_dir = "output"
+def convert_text_to_bear_audio(input_text):
+    int_dir = "./intermediate_files"
+    out_dir = "./output"
 
-# Check if the directory already exists
-if not os.path.exists(int_dir):
-    # If it doesn't exist, create the directory
-    os.makedirs(int_dir)
+    # Create the intermediate directory if it doesn't already exist
+    if not os.path.exists(int_dir):
+        os.makedirs(int_dir)
 
-if not os.path.exists(out_dir):
-    # If it doesn't exist, create the directory
-    os.makedirs(out_dir)
+    # Create the output directory if it doesn't already exist
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-# Text-to-speech and save as WAV
-tts = gTTS(input_text)
-tts.save("./intermediate_files/raw_audio.mp3")
+    audio_mp3_path = int_dir + "/raw_audio.mp3"
+    audio_wav_path = int_dir + "/raw_audio.wav"
 
-# Convert MP3 to WAV using PyDub
-audio = AudioSegment.from_mp3("./intermediate_files/raw_audio.mp3")
-audio.export("./intermediate_files/raw_audio.wav", format="wav")
+    # Text-to-speech and save as WAV
+    tts = gTTS(input_text)
+    tts.save(audio_mp3_path)
 
-#Read in raw wave file
-filename = './intermediate_files/raw_audio.wav'
-sound = AudioSegment.from_file(filename, format=filename[-3:])
+    # Convert MP3 to WAV using PyDub
+    audio = AudioSegment.from_mp3(audio_mp3_path)
+    audio.export(audio_wav_path, format="wav")
 
-#Shift the audio up
-octaves = 0.4
-new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
-hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
-hipitch_sound = hipitch_sound.set_frame_rate(44100)
+    # Read in raw wave file
+    sound = AudioSegment.from_file(audio_wav_path, format=audio_wav_path[-3:])
 
-#export / save pitch changed sound
-hipitch_sound.export(f"./output/bear_voice.wav", format="wav")
+    # Shift the audio up
+    octaves = 0.4
+    new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+    hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+    hipitch_sound = hipitch_sound.set_frame_rate(44100)
+
+    # Export pitch-changed sound
+    hipitch_sound.export(out_dir + "/bear_voice.wav", format="wav")
+
+    # Remove the intermediate directory
+    os.remove(audio_mp3_path)
+    os.remove(audio_wav_path)
+    os.rmdir(int_dir)
+
+if __name__ == '__main__':
+
+    input_text = "Hi there! What's your name? My name is foo foo the bear. Would you like to play a choose your own adventure game?"
+
+    convert_text_to_bear_audio(input_text)
