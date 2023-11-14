c7c9069c73d1df12a9b3ee72ace5801906f3351f
Nick Brandis <niklbrandis@gmail.com>
2023-11-13 18:36:40 -0800

    make api call from game.py

--- game.py
+++ game.py
@@ -1,16 +1,12 @@
 import speech_processing as sp
 import text_to_bear_audio as tba
-import openai
-import time
-
-openai.api_key = user_secrets.get_secret("OPENAI_API_KEY")
-startime = time.time()
+import stream_test as st
 
 def main():
     speech = sp.gather_speech_data()
     print("you said: " + speech)
 
-    # TODO: Make API call
+    st.make_api_call(speech)
 
     tba.convert_text_to_bear_audio(speech)
 
--- stream_test.py
+++ stream_test.py
@@ -1,51 +1,30 @@
 
-# from openai import OpenAI
-# import time
-# import os
-
-# client = OpenAI()
-
-# startime = time.time()
-
-# ### STREAM CHATGPT API RESPONSES
-# delay_time = 0.01 #  faster
-# max_response_length = 200
-# answer = ''
-# # ASK QUESTION
-# prompt = input("Ask a question: ")
-# start_time = time.time()
-
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-#   ]
-# )
-
-# print(completion.choices[0].message)
-
 from openai import OpenAI
 import time
 
-#defaults to getting the key using os.environ.get("OPENAI_API_KEY") in .env file
-client = OpenAI()
+def make_api_call(user_prompt):
+    #defaults to getting the key using os.environ.get("OPENAI_API_KEY") in .env file
+    client = OpenAI()
 
-delay_time = 0.01 #  faster
-max_response_length = 200
-answer = ''
-#ASK QUESTION
-start_time = time.time()
+    delay_time = 0.01 #  faster
+    max_response_length = 200
+    answer = ''
+    #ASK QUESTION
+    start_time = time.time()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  stream=True,
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a 10 word poem."}
-  ]
-)
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    stream=True,
+    messages=[
+        {"role": "system", "content": "You are the master of a choose your own adventure game where the player is lost in a forest. This is what he says:"},
+        {"role": "user", "content": user_prompt + "Compose a response in less than 50 words"}
+    ]
+    )
 
-#STREAM THE ANSWER
-for event in response: 
-    print(event.choices[0].delta.content)
+    #STREAM THE ANSWER
+    for event in response: 
+        print(event.choices[0].delta.content)
+
+if __name__ == '__main__':
+
+    make_api_call("what's the capital of djibouti?")
