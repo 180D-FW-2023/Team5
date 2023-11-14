bd6b32024b9474a9f8dfd4fd671081dbeefef666
Spencer Stice <smstice17@gmail.com>
2023-11-14 01:20:15 -0800

    Implemented dynamic stream to audio conversion to minimize intial latency and pauses in speech. More experimentation with parameters needed.

--- llm_handler.py
+++ llm_handler.py
@@ -1,19 +1,73 @@
 from openai import OpenAI
+import string
+import text_to_bear_audio as tba
+import concurrent.futures
+import time
+import pygame
 
 # defaults to getting the key using os.environ.get("OPENAI_API_KEY") in .env file
 client = OpenAI()
+pygame.init()
+pygame.mixer.init()
 
-#Requests a response from GPT 3.5 Turbo with streaming ENABLED
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  stream=True,
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a 50 word poem."}
-  ]
-)
+def has_punctuation(input_string):
+    return any(char in string.punctuation for char in input_string)
 
-# STREAM THE ANSWER
-for event in response: 
-    print(event.choices[0].delta.content)
+def translate_and_get_duration(trans_block, output_path):
+    print('-----------')
+    print(trans_block)
+    print('-----------')
+    duration, final_path = tba.convert_text_to_bear_audio(trans_block, output_path)
+    return duration, final_path
 
+def send_api_request(prompt, P=20, translation_time=2):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        stream=True,
+        messages=[
+            {"role": "system", "content": "You are a child's favorite teddy bear. Always respond with exactly 100 words."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    pieces = []
+    full = ''
+    audio_count = 0
+    required_time = 1e10
+    first = True
+    last_start_time = 0
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for event in response:
+            curr_piece = event.choices[0].delta.content
+            print(curr_piece)
+            if not isinstance(curr_piece, str):
+                if len(pieces) > 0:
+                    trans_block = ''.join(pieces)
+                    required_time, final_path = translate_and_get_duration(trans_block, audio_count)
+                    executor.submit(tba.play_wav, final_path)
+            else:
+                if curr_piece == '':
+                    continue
+                
+                pieces.append(curr_piece)
+                
+                if (first and len(pieces) >= P + 1) or (time.time() - last_start_time > required_time + 1 - translation_time) and not has_punctuation(curr_piece):
+                    first = False
+                    trans_block = ''.join(pieces[:-1])
+                    required_time, final_path = translate_and_get_duration(trans_block, audio_count)
+                    print(required_time)
+                    #time.sleep(2)
+                    last_start_time = time.time()
+                    executor.submit(tba.play_wav, final_path)
+                    audio_count += 1
+                    pieces = [pieces[-1]]
+                    
+
+    print(full)
+
+if __name__ == '__main__':
+    input_text = '''
+        I like to eat pizza, do you?
+    '''
+    send_api_request(input_text)
--- text_to_bear_audio.py
+++ text_to_bear_audio.py
@@ -18,23 +18,29 @@ import wave
 import os
 import pygame
 import time
+import threading
+
+# Create a lock to synchronize access to the audio playback
+audio_lock = threading.Lock()
+pygame.init()
+pygame.mixer.init()
 
 # Use pygame to play a wav file using default speaker
+# Use pygame to play a wav file using default speaker
 def play_wav(wav_path):
-    pygame.init()
-    pygame.mixer.init()
+    with audio_lock:
 
-    try:
-        sound = pygame.mixer.Sound(wav_path)
-        sound.play()
-        time.sleep(sound.get_length())  # Wait for the sound to finish playing
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        pygame.quit()
+        try:
+            sound = pygame.mixer.Sound(wav_path)
+            sound.play()
+            # Wait for the sound to finish playing
+            pygame.time.wait(int(sound.get_length() * 1000))
+        except Exception as e:
+            print(f"Error: {e}")
+
 
 # Creates a bear voice wav file and plays it
-def convert_text_to_bear_audio(input_text):
+def convert_text_to_bear_audio(input_text, output_path):
     int_dir = "./intermediate_files"
     out_dir = "./output"
 
@@ -46,8 +52,8 @@ def convert_text_to_bear_audio(input_text):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    audio_mp3_path = int_dir + "/raw_audio.mp3"
-    audio_wav_path = int_dir + "/raw_audio.wav"
+    audio_mp3_path = int_dir + "/raw_audio_ " + str(output_path) + ".mp3"
+    audio_wav_path = int_dir + "/raw_audio_" + str(output_path) + ".wav"
 
     # Text-to-speech and save as WAV
     tts = gTTS(input_text)
@@ -67,14 +73,16 @@ def convert_text_to_bear_audio(input_text):
     hipitch_sound = hipitch_sound.set_frame_rate(44100)
 
     # Export pitch-changed sound
-    pitch_change_path = out_dir + "/bear_voice.wav"
+    pitch_change_path = out_dir + "/bear_voice_" + str(output_path) + ".wav"
     hipitch_sound.export(pitch_change_path, format="wav")
-    play_wav(pitch_change_path)
 
     # Remove the intermediate directory
     os.remove(audio_mp3_path)
     os.remove(audio_wav_path)
     os.rmdir(int_dir)
+
+    sound = pygame.mixer.Sound(pitch_change_path)
+    return sound.get_length(), pitch_change_path
 
 if __name__ == '__main__':
 
