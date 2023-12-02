0e63c347b256fb892e2d3ca6f3b197f7ce4c2733
William Huang <whuang37@ucla.edu>
2023-12-01 19:22:57 -0800

    fixed typo for test on tba

--- experiments/end_to_end_speech_test.py
+++ experiments/end_to_end_speech_test.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append(r"/home/pi/Team5/src")
 
 import rbp_drivers.wm8960_helpers as rbp
@@ -6,8 +7,6 @@ import speech_processing as sp
 import text_to_bear_audio as tba
 
 import time # timing tests
-
-
 
 # helper function to test timings
 def timeit(func):
@@ -25,12 +24,11 @@ def main():
     timeit(rbp.record_audio_by_time)(temp_wav_file, record_time=10)
     text = timeit(sp.recognize_wav)(temp_wav_file)
 
-    out_audio_path = timeit(tba.convert_text_to_bear_audio)(text)
+    os.remove("out.wav")
+
+    out_audio_path = timeit(tba.convert_text_to_bear_audio)(text, 1)
     print(out_audio_path)
     timeit(rbp.play_audio)(out_audio_path)
-
-
-
 
 if __name__ == "__main__":
     main()
