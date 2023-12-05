d2629e5c7b89d62f01943d67edf769afcfcd5a38
William Huang <whuang7337@gmail.com>
2023-12-05 01:33:46 -0800

    moved recording to a state class

--- src/game_client.py
+++ src/game_client.py
@@ -31,15 +31,18 @@ class GameClient:
         #  client setup
         self.ftc = tcp.FileTransferClient(server_ip, server_port) # connects to server in init
         self.ftc.connect_to_server()
+        
+        self.recorder = rbp.Recorder()
     
     def main_loop_non_stream(self):
         while True:
             received_file_path = str(self.temp_dir / "received.wav")
             self.ftc.receive_file(received_file_path)
             rbp.play_audio(received_file_path)
+
             record_file_path = str(self.temp_dir / "recorded.wav")
-            time.sleep(.5)
-            record_file_path = rbp.record_audio_by_time(record_file_path)
+
+            record_file_path = self.recorder.record_audio_by_time(record_file_path)
             self.ftc.send_file(record_file_path)
 
     def __del__(self):
--- src/rbp_drivers/wm8960_helpers.py
+++ src/rbp_drivers/wm8960_helpers.py
@@ -2,70 +2,13 @@ from __future__ import print_function
 
 import time
 import wave
-import alsaaudio
+import pyaudio
 
-
-CHANNELS = 1
-RATE = 16000
-FORMAT = alsaaudio.PCM_FORMAT_S16_LE
-PERIOD_SIZE = 160
-
-FORMAT_DICT = {alsaaudio.PCM_FORMAT_S16_LE: 2}
-
-def get_input_device(device="default"):
-    # Open the device in nonblocking capture mode. The last argument could
-    # just as well have been zero for blocking mode. Then we could have
-    # left out the sleep call in the bottom of the loop
-    inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,
-                        alsaaudio.PCM_NONBLOCK,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        format=FORMAT,
-                        periodsize=PERIOD_SIZE,
-                        device=device)
-    # The period size controls the internal number of frames per period.
-    # The significance of this parameter is documented in the ALSA api.
-    # For our purposes, it is suficcient to know that reads from the device
-    # will return this many frames. Each frame being 2 bytes long.
-    # This means that the reads below will return either 320 bytes of data
-    # or 0 bytes of data. The latter is possible because we are in nonblocking
-    # mode.
-    inp.setperiodsize(PERIOD_SIZE)
-    return inp
-
-def get_saved_audio_file(output_file_path):
-    output_file_path = str(output_file_path)
-    f = wave.open(output_file_path, "wb")
-    f.setnchannels(CHANNELS)
-    f.setsampwidth(FORMAT_DICT[FORMAT])
-    f.setframerate(RATE)
-
-    print(f'Input Device: {f.getnchannels()} channels, {f.getframerate()} sampling rate\n')
-    return f
-
-# def record_audio_by_time(output_file_path, device="default", record_time=10):
-#     # rough time in s
-#     inp = get_input_device(device)
-
-#     f = get_saved_audio_file(output_file_path)
-
-#     start_time = time.time()
-
-#     first_flag = True
-#     while True:
-#         l, data = inp.read()
-#         if first_flag:
-#             first_flag = False
-#             start_time = time.time()
-#         if l:
-#             f.writeframes(data)
-#             time.sleep(.001)
-#             # saves exactly time ms
-#         if time.time() - start_time > record_time:
-#             break
-#     f.close()
-
-#     return output_file_path
+# Setup channel info
+FORMAT = pyaudio.paInt16 # data type formate
+CHANNELS = 1 # Adjust to your number of channels
+RATE = 16000 # Sample Rate
+CHUNK = 1024 # Block Size
 
 def open_audio_file(path):
     path = str(path)
@@ -78,76 +21,40 @@ def open_audio_file(path):
 
     return f
 
-# def play_audio(path, device="default"):
-#     f = open_audio_file(path)
-#     if f is None:
-#         return False # failed to play
+class Recorder:
+    def __init__(self):
+        self.audio = pyaudio.PyAudio()
 
-#     # 8bit is unsigned in wav files
-#     if f.getsampwidth() == 1:
-#         format = alsaaudio.PCM_FORMAT_U8
-#     # Otherwise we assume signed data, little endian
-#     elif f.getsampwidth() == 2:
-#         format = alsaaudio.PCM_FORMAT_S16_LE
-#     elif f.getsampwidth() == 3:
-#         format = alsaaudio.PCM_FORMAT_S24_LE
-#     elif f.getsampwidth() == 4:
-#         format = alsaaudio.PCM_FORMAT_S32_LE
-#     else:
-#         raise ValueError('Unsupported format')
-
-#     periodsize = f.getframerate() // 8
-
-#     out = alsaaudio.PCM(channels=f.getnchannels(),
-#                            rate=f.getframerate(),
-#                            format=format,
-#                            periodsize=periodsize,
-#                            device=device)
-#     data = f.readframes(periodsize)
-#     while data:
-#         # Read data from stdin
-#         out.write(data)
-#         data = f.readframes(periodsize)
-
-#     f.close()
-#     return True
-
-import pyaudio
-# Setup channel info
-FORMAT = pyaudio.paInt16 # data type formate
-CHANNELS = 1 # Adjust to your number of channels
-RATE = 16000 # Sample Rate
-CHUNK = 1024 # Block Size
-WAVE_OUTPUT_FILENAME = "out.wav"
-
-def record_audio_by_time(output_file_path, device="default", record_time=10):
-    # Startup pyaudio instance
-    audio = pyaudio.PyAudio()
-
-    # start Recording
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
+        # start Recording
+        self.stream = self.audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
                     frames_per_buffer=CHUNK)
-    frames = []
 
-    # Record for RECORD_SECONDS
-    for i in range(0, int(RATE / CHUNK * record_time)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+    def __del__(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+
+    def record_audio_by_time(self, output_file_path, record_time=10):
+        frames = []
+
+        # Record for RECORD_SECONDS
+        for i in range(0, int(RATE / CHUNK * record_time)):
+            data = self.stream.read(CHUNK)
+            frames.append(data)
 
 
-    # Stop Recording
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+        # Stop Recording
 
-    # Write your new .wav file with built in Python 3 Wave module
-    waveFile = wave.open(output_file_path, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(frames))
-    waveFile.close()
+        # Write your new .wav file with built in Python 3 Wave module
+        waveFile = wave.open(output_file_path, 'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(self..get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(frames))
+        waveFile.close()
+
+        return output_file_path
 
 def play_audio(path, device="default"):
     f = open_audio_file(path)
@@ -178,5 +85,4 @@ def play_audio(path, device="default"):
 
 
 if __name__ == "__main__":
-    record_audio_by_time("out.wav")
     play_audio("out.wav")
