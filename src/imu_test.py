1ae9b222d3d5bc342f685171f9f853678f65c15a
Nick Brandis <niklbrandis@gmail.com>
2024-02-27 10:47:30 -0800

    added imu shake functionality

--- src/imu_test.py
+++ src/imu_test.py
@@ -5,12 +5,21 @@ import time
 imu = ImuHandler()
 
 while True:
-    imu_res = imu.collect_imu_data()
-    if imu_res == False:
-        print("\nleft left left left\n")
-    elif imu_res == True:
-        print("\nright right right right\n")
-    # Return value was None
+    # Left/right Test
+    # imu_res = imu.collect_imu_data()
+    # if imu_res == False:
+    #     print("\nleft left left left\n")
+    # elif imu_res == True:
+    #     print("\nright right right right\n")
+    # # Return value was None
+    # else:
+    #     print("\nno turn detected\n")
+
+    # Shake Test
+    imu_res = imu.detect_shake()
+    if imu_res:
+        print("SHAKE")
     else:
-        print("\nno turn detected\n")
+        print("no shake")
+        
     time.sleep(3)
